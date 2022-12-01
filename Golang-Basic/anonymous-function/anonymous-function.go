1814a35a689aac3c22b4c6c20f5f31d9c8ee7051
Nur Fatchurohman <nur.faturohman28@gmail.com>
2022-12-01 14:01:15 +0700

    anonymous function

--- Golang-Basic/anonymous-function/anonymous-function.go
+++ Golang-Basic/anonymous-function/anonymous-function.go
@@ -2,22 +2,21 @@ package main
 
 import "fmt"
 
-type Blacklist func(string) bool
+type Blacklist func(string)bool
 
 func registerUser(name string, blacklist Blacklist) {
 	if blacklist(name) {
-		fmt.Println("you are blocked!", name)
+		fmt.Println("you are blocked ", name)
 	} else {
-		fmt.Println("Welcome", name)
+		fmt.Println("Welcome ", name)
 	}
 }
 
 func main() {
 	blacklist := func(name string) bool {
-		// fmt.Println(name == "user")
 		return name == "user"
 	}
 
-	registerUser("fatur", blacklist)
-	registerUser("admin", blacklist)
+	registerUser("admin", blacklist )
+	registerUser("user", blacklist )
 }
