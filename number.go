c0d8c58916c8e249fdb7f5883aca36a6b5ab1b2a
Nur Fatchurohman <nur.faturohman28@gmail.com>
2022-12-01 13:51:53 +0700

    change structure

--- number.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import "fmt"
-
-func number(){
-	fmt.Print("Satu = ", 1)
-	fmt.Print("Dua = ", 2)
-	fmt.Print("Tiga koma lima = ", 3.5)
-}
--- string.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func strings(){
-	fmt.Println(len("Fatur"))
-	fmt.Println("Fatur"[0])
-}
