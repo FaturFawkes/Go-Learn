d3c26b2893d62e892b9b7109e3a2ce644af86ffe
Nur Fatchurohman <nur.faturohman28@gmail.com>
2022-12-01 14:10:32 +0700

    function recursive

--- Golang-Basic/function-recursive/function-recursive.go
+++ Golang-Basic/function-recursive/function-recursive.go
@@ -3,27 +3,25 @@ package main
 import "fmt"
 
 // MENGGUNAKAN FOR LOOP
-func faktorialLoop(value int)int{
+func faktorialLoop(value int) int {
 	result := 1
-	for i := value; i >0 ; i-- {
+	for i := value; i > 0; i-- {
 		result *= i
 	}
 	return result
 }
 
 // MENGGUNAKAN RECURSIVE FUNCTION
-func faktorialResursive(value int)int {
-	if value == 1{
+func faktorialRecursive(val int) int {
+	if val == 1 {
 		return 1
 	} else {
-		return value * faktorialResursive(value-1)
+		return val * faktorialRecursive(val-1)
 	}
 }
 
 func main() {
-	hasil := faktorialLoop(5)
-	fmt.Println(hasil)
-
-	fmt.Println(faktorialResursive(5))
+	fmt.Println(faktorialLoop(20))
+	fmt.Println(faktorialRecursive(20))
 }
 
