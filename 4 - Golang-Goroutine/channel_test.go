757fbbea2afd1e2026f84bcd4690c3b357a2685c
Nur Fatchurohman <nur.faturohman28@gmail.com>
2022-12-13 15:39:17 +0700

    buffered channel

--- 4 - Golang-Goroutine/channel_test.go
+++ 4 - Golang-Goroutine/channel_test.go
@@ -26,16 +26,52 @@ func TestCreateChannel(t *testing.T) {
 func GiveMeResponse(channel chan string) {
 	time.Sleep(2 * time.Second) 
 	channel <- "Bnox"
-
 }
 
+// Channel as parameter
 func TestChannelAsParameter(t *testing.T) {
 	channel := make(chan string)
 	defer close(channel)
 
 	go GiveMeResponse(channel)
 	
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
 	data := <-channel
 	fmt.Println(data)
 	time.Sleep(5 * time.Second)
+}
+
+// Channel in/out only
+
+// Only in
+func OnlyIn(channel chan<- string) {
+	channel <- "Fatur"
+}
+
+// Only out
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+// Buffered Channel
+func TestBufferedChannel(t *testing.T) {
+	channel := make(chan string, 3)
+	defer close(channel)
+
+	go func ()  {
+		channel <- "Nur"
+		channel <- "Fatur"
+		channel <- "Rohman"
+	}()
+	
+	go func ()  {
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+		fmt.Println(<-channel)
+	}()
+
+	time.Sleep(2 * time.Second)
 }
