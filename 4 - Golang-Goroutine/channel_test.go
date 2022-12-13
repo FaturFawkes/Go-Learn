8cc2865decd8de8b0d3731bf1d001f07d57f570b
Nur Fatchurohman <nur.faturohman28@gmail.com>
2022-12-13 15:00:20 +0700

    channel as parameter

--- 4 - Golang-Goroutine/channel_test.go
+++ 4 - Golang-Goroutine/channel_test.go
@@ -22,3 +22,20 @@ func TestCreateChannel(t *testing.T) {
 	fmt.Println(data)
 	time.Sleep(5 * time.Second)
 }
+
+func GiveMeResponse(channel chan string) {
+	time.Sleep(2 * time.Second) 
+	channel <- "Bnox"
+
+}
+
+func TestChannelAsParameter(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go GiveMeResponse(channel)
+	
+	data := <-channel
+	fmt.Println(data)
+	time.Sleep(5 * time.Second)
+}
