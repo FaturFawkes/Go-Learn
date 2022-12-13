b51b5741d3ee7a3ad2966932ebc9e875aade9c8d
Nur Fatchurohman <nur.faturohman28@gmail.com>
2022-12-13 11:35:34 +0700

    loop goroutine

--- 4 - Golang-Goroutine/belajar-goroutine/goroutine_test.go
+++ 4 - Golang-Goroutine/belajar-goroutine/goroutine_test.go
@@ -10,9 +10,20 @@ func RunHelloWorld() {
 	fmt.Println("Hello World!")
 }
 
+func DisplayNumber(num int) {
+	fmt.Println("Display - ", num)
+}
+
 func TestCreateGoroutine(t *testing.T) {
 	go RunHelloWorld()
 	fmt.Println("Halo kambing")
 
 	time.Sleep(1 * time.Second)
+}
+
+func TestManyGoroutine(t *testing.T) {
+	for i := 0; i < 100000; i++ {
+		go DisplayNumber(i)
+	}
+	time.Sleep(5 * time.Second)
 }
