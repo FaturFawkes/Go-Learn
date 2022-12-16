4c9275d1bbfb368930af63e7ef230ecb8a9c463c
Nur Fatchurohman <nur.faturohman28@gmail.com>
2022-12-16 10:49:03 +0700

    atomic

---
+++ 4 - Golang-Goroutine/atomic_test.go
@@ -0,0 +1,25 @@
+package belajargoroutine
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+	"testing"
+)
+
+func TestAtomic(t *testing.T) {
+	var x int64
+	group := &sync.WaitGroup{}
+
+	for i := 1; i <= 1000; i++ {
+		go func() {
+			defer group.Done()
+			group.Add(1)
+			for j := 1; j <= 100; j++ {
+				atomic.AddInt64(&x, 1)
+			}
+		}()
+	}
+	group.Wait()
+	fmt.Println("Counter = ", x)
+}
